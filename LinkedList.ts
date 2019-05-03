7af3e338771a64156dfbbfb694a4619d82941b2c
Viveksanthosh <viveksanthosh@holidayiq.com>
2019-05-03 14:58:28 +0530

    added add to end

--- LinkedList.ts
+++ LinkedList.ts
@@ -1,15 +1,31 @@
 class LinkNode {
-    constructor(private data, private link: LinkNode | null) {
-
+    data: any = null;
+    link: LinkNode | null = null;
+    constructor(data, link: LinkNode | null) {
+        this.data = data;
+        this.link = link;
     }
 }
 
 class LinkedList {
     head: LinkNode | null = null;
-    
+
     addToStart(data) {
         const node = new LinkNode(data, this.head);
         this.head = node;
+    }
+
+    addToEnd(data) {
+        const newNode = new LinkNode(data, null)
+        if (this.head === null)
+            this.head = newNode;
+        else {
+            let node = this.head;
+            while (node.link) {
+                node = node.link
+            }
+            node.link = newNode;
+        }
     }
 }
 
@@ -17,4 +33,7 @@ let list = new LinkedList();
 list.addToStart(2);
 list.addToStart(1);
 
-console.log(list)
+console.dir(list);
+list.addToEnd(3);
+console.log('---------');
+console.dir(JSON.stringify(list));
